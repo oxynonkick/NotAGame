12e9cd1de2859f7f3ca92303ae56a97e7bb0767e
EdgarMertens <144oxygenfx@gmail.com>
2014-02-20 18:46:16 +0200

    Platforms
    Добавляет префаб для платфор, скрипт для них же. Еще српайтики для
    платформ луга, десу.

---
+++ MobileGame/assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformGenerator : MonoBehaviour 
+{
+
+	public GameObject PlatformUnit;
+	public int Height;
+	public int Width;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Vector3 sizeOfUnit = PlatformUnit.renderer.bounds.size;
+		for(int i = 0; i < Width; i++)
+			for(int j = 0; j < Height; j++)
+			{
+				Vector3 unitPosition = transform.position;
+				unitPosition.x += i*sizeOfUnit.x;
+				unitPosition.y += j*sizeOfUnit.y;
+				GameObject unit = Instantiate(PlatformUnit, unitPosition, transform.rotation) as GameObject;
+				unit.transform.parent = transform;				
+			}
+	}
+}
